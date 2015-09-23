af97a005e3b4edaafd0b12acf3efa70e0d7749fc
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-09-23 15:48:39 -0700

    Update & cleanup Metronome class
    Added private variable constants. Though I may want to move this to the
    parent class, Ticker.
    
    Update the conversion methods. They were innaccurate before, but should
    work for simple cases now. I need to research what happens when
    floating point numbers are passed to 'setInterval', since those types
    of numbers are frequently generated.
    
    Add & remove some comments for update methods.
    
    Add event listener to properly set 'Metronome.bpm' property whenever
    'Metronome.ms' changes.
    
    Add basic unit test specs for new functionality.

--- spec/metronome_spec.js
+++ spec/metronome_spec.js
@@ -7,33 +7,67 @@ describe('Metronome', function() {
   describe('#fromBPM', function() {
     var m = new Metronome();
 
-    it('returns the number of milliseconds per one beat', function() {
-      assert.equal(m.fromBPM(30), 500);
-      assert.equal(m.fromBPM(60), 1000);
-      assert.equal(m.fromBPM(120), 2000);
+    context('when tempo is 30 bpm', function() {
+      it('one beat is 2000 milliseconds', function() {
+        assert.equal(m.fromBPM(30), 2000);
+      });
+    });
+    context('when tempo is 60 bpm', function() {
+      it('one beat is 1000 milliseconds', function() {
+        assert.equal(m.fromBPM(60), 1000);
+      });
+    });
+    context('when tempo is 120 bpm', function() {
+      it('one beat is 500 milliseconds', function() {
+        assert.equal(m.fromBPM(120), 500);
+      });
     });
   });
 
   describe('#toBPM', function() {
     var m = new Metronome();
 
-    it('returns the beats per minute', function() {
-      assert.equal(m.toBPM(), 60);
-      assert.equal(m.bpm, 60);
+    context('when one beat is 2000 milliseconds', function() {
+      it('returns tempo of 30 bpm', function() {
+        assert.equal(m.toBPM(2000), 30);
+      });
     });
-    it('returns the beats per minute', function() {
-      m.set(2000);
-      assert.equal(m.toBPM(), 120);
-      assert.equal(m.bpm, 120);
+    context('when one beat is 1000 milliseconds', function() {
+      it('returns tempo of 60 bpm', function() {
+        assert.equal(m.toBPM(1000), 60);
+      });
+    });
+    context('when one beat is 500 milliseconds', function() {
+      it('returns tempo of 120 bpm', function() {
+        assert.equal(m.toBPM(500), 120);
+      });
     });
   });
 
-  xdescribe('changing the tempo', function() {
+  describe('changing the tempo', function() {
     var m = new Metronome();
 
-    it('can change its tempo', function() {
-      m.set();
-      assert.equal();
+    context('setup: checking default settings', function() {
+      it('bpm is 60', function() {
+        assert.equal(m.bpm, 60);
+      });
+      it('one beat takes 1000 ms', function() {
+        assert.equal(m.ms, 1000);
+      });
+    });
+
+    context('after a tempo change', function() {
+
+      beforeEach(function setNewBPM() {
+        m.set(m.fromBPM(75)); // number of milliseconds for one beat at 75 bpm
+      });
+
+      it('bpm is 75', function() {
+        assert.equal(m.bpm, 75);
+      });
+      it('one beat takes 800 ms', function() {
+        assert.equal(m.ms, 800);
+      });
     });
   });
 });
