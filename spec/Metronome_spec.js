b8260defba2043089f69d52d5500b731a8615015
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-09-24 14:42:13 -0700

    Change Metronome#set, practice BDD other misc updates. Squashed commit of the following:
    commit ec65169d3a5a976e60427b900c0ea0a381518315
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 14:38:06 2015 -0700
    
        Clarify Metronome spec during tempo changes
    
        Add some helpful comments regarding purpose of the artifically-set
        timeouts. Also reword the phrases used in the 'it' blocks because I'm
        not actually measuring the time it takes for one beat to finish.
        Instead, I'm checking a property on the object.
    
    commit 24df342744ae5ce3373976ec0886e3b3eeb5877a
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 14:09:18 2015 -0700
    
        Asynchronously spec states on Metronome object
    
        Ensure that the metronome has a chance to run before moving on the next
        assertion.
    
        The 'done' callback must be passed into each 'it' block. Then, it must
        be called from inside the callback to 'setTimeout'.
    
        I tried several other ways, but the test runner kept moving past it.
        Sometimes the output would be on the bottom, or mixed in with output
        from another file.
    
        When I run the specs now, it waits for the timeout to pass before
        evaluating the current assertion & moving on to the next assertion;
        as if it was synchronous.
    
    commit f76b21d053cb681f42286bbad969cf882457c0b1
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 14:04:28 2015 -0700
    
        Cleanup a block in Metronome specs
    
        Within the 'changing the tempo' context block, give each context its
        own clean copy of a metronome object.
    
        Add a comment to clarify the units expected by 'Metronome#set' method.
    
        set & reset bpm before & after specs in a block.
    
    commit 5482b0254ca0b1045fbb0d38ef7977f8eae98bed
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 12:02:47 2015 -0700
    
        Metronome#set takes bpm instead of ms
    
    commit f125aa9a9dc4bcd9a87d1642d106b9a8f29ca193
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 11:18:59 2015 -0700
    
        Pass a reference to callback in setInterval
    
        This will make for slightly cleaner stack traces. Was also recommended
        by the node JS style guide, written by felixge.
    
    commit 7451db6c7e05e032bd87a740f59fc0ca35f44d4c
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 11:17:51 2015 -0700
    
        Fix typo in 'start' event listener
    
        Rename the callback in this event listener.

--- spec/Metronome_spec.js
+++ spec/Metronome_spec.js
@@ -45,29 +45,97 @@ describe('Metronome', function() {
   });
 
   describe('changing the tempo', function() {
-    var m = new Metronome();
 
     context('setup: checking default settings', function() {
+      var m = new Metronome();
       it('bpm is 60', function() {
         assert.equal(m.bpm, 60);
       });
-      it('one beat takes 1000 ms', function() {
+      it('one beat is set to 1000 ms', function() {
         assert.equal(m.ms, 1000);
       });
     });
 
-    context('after a tempo change', function() {
+    context('states after a tempo change', function() {
+      var m = new Metronome();
+
+      // Metronome#set interprets the parameter as `bpm` instead of `ms`.
 
       beforeEach(function setNewBPM() {
-        m.set(m.fromBPM(75)); // number of milliseconds for one beat at 75 bpm
+        m.set(75);
+      });
+
+      afterEach(function resetBPM() {
+        m.set(60);
       });
 
       it('bpm is 75', function() {
         assert.equal(m.bpm, 75);
       });
-      it('one beat takes 800 ms', function() {
+      it('one beat is set to 800 ms', function() {
         assert.equal(m.ms, 800);
       });
     });
+
+    context('metronome states during tempo changes', function() {
+      var m = new Metronome();
+      // Artificially set time between assertions.
+      // This is to emulate a simple scenario.
+      // Typically, if you make Mocha wait longer than 2000 ms, it will give up.
+      // But there are command-line options for Mocha to increase the limit.
+      var msForTesting = 1000;
+
+      before('set to 120 & start', function() {
+        m.set(120);
+        m.start();
+      });
+
+      context('starting at 120 bpm', function() {
+        it('bpm is 120', function(done) {
+          setTimeout(function() {
+            assert.equal(m.bpm, 120);
+            // Ensure that the timeout is respected by calling
+            // `done` inside the callback to `setTimeout`.
+            // Otherwise, the test suite will move on to evaluate the wrong
+            // assertion under the wrong `it` block & provide false positives.
+            done();
+          }, msForTesting);
+        });
+        it('one beat is set to 500 ms', function(done) {
+          setTimeout(function() {
+            assert.equal(m.ms, 500);
+            done();
+          }, msForTesting);
+        });
+      });
+
+      context('setting back to 60 bpm', function() {
+        it('bpm is 60', function(done) {
+          m.set(60);
+          setTimeout(function() {
+            assert.equal(m.bpm, 60);
+            done();
+          }, msForTesting);
+        });
+        it('one beat is set to 1000 ms', function(done) {
+          m.set(60);
+          setTimeout(function() {
+            assert.equal(m.ms, 1000);
+            done();
+          }, msForTesting);
+        });
+      });
+
+      context('stopping metronome', function() {
+        it('deletes the ticker property from the metronome', function(done) {
+          setTimeout(function() {
+            m.stop();
+            assert(!m.hasOwnProperty('ticker'));
+            done();
+          }, msForTesting);
+        });
+      });
+    });
+
   });
 });
