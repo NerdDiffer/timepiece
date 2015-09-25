234f942c7106db0c72a5a10c0c7fc43ce9ee3a98
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-09-24 19:27:37 -0700

    Update Timepiece class. Squashed commit of the following:
    commit b57ad4adb74946e7f67b7b0e54be063a987a0044
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 19:20:09 2015 -0700
    
        Do nothing in Timepiece#start if already active
    
        Guard against two active calls to 'setInterval' in the same object by
        first checking if the object is active or not.
    
    commit a825b8c85ad715076c83e66021706d101c518d88
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 19:17:57 2015 -0700
    
        Add some pretty thorough specs to Timepiece
    
        Checking all possible pathways through the prototype methods of
        Timepiece, since its the base class.
    
    commit 056857512441dfbde075401e8a1285f0e74eebfc
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 17:41:10 2015 -0700
    
        Use new #isActive method in two event listeners of Timepiece
    
    commit 41f296fab9ee7c9f611073022d0b563278721849
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Thu Sep 24 17:31:49 2015 -0700
    
        Add an '#isActive' method to Timepiece.prototype
    
        This will come in handy down the line.

--- spec/Timepiece_spec.js
+++ spec/Timepiece_spec.js
@@ -34,16 +34,126 @@ describe('Timepiece', function() {
     });
   });
 
+  describe('#isActive', function() {
+    it('returns true if the ticker is running', function() {
+      var t = new Timepiece();
+      t.start();
+      assert(t.isActive());
+    });
+    it('returns false if the ticker is not running', function() {
+      var t = new Timepiece();
+      assert(!t.isActive());
+    });
+  });
+
   describe('#start', function() {
 
+    it('emits a "start" event', function() {
+      var t = new Timepiece();
+
+      var started = false;
+      t.once('start', function() {
+        started = true;
+      });
+
+      t.start();
+      assert(started);
+      t.stop();
+    });
+    context('when timer is not running', function() {
+      var t = new Timepiece();
+
+      var started = false;
+      t.once('start', function() {
+        started = true;
+      });
+
+      it('starts the timer', function() {
+        t.start();
+        assert(t.isActive());
+        t.stop();
+      });
+    });
   });
 
   describe('#stop', function() {
 
+    it('emits a "stop" event', function() {
+      var t = new Timepiece();
+      var fired = false;
+      t.once('stop', function() {
+        fired = true;
+      });
+      t.stop();
+      assert(fired);
+    });
+
+    context('when timer is already running', function() {
+      var t = new Timepiece();
+
+      before('setup: start the timer', function() {
+        t.start();
+      });
+      it('stops the timer', function() {
+        t.stop();
+        assert(!t.isActive());
+      });
+    });
   });
 
   describe('#set', function() {
 
+    context('whether or not the timer is running', function() {
+      var t = new Timepiece();
+
+      var wasSet = false;
+      t.once('set', function() {
+        wasSet = true;
+      });
+
+      before('set a new value for ms', function() {
+        t.set(1234);
+      });
+
+      it('emits a "set" event', function() {
+        assert(wasSet);
+      });
+      it('sets a new value of the "ms" property', function() {
+        assert.equal(t.ms, 1234);
+      });
+    });
+    context('if timer is already running', function() {
+      var t = new Timepiece();
+      var timesStarted = 0;
+      var timesStopped = 0;
+
+      t.on('start', function() {
+        timesStarted++;
+      });
+      t.on('stop', function() {
+        timesStopped++;
+      });
+
+      before('start me up!', function() {
+        t.start();
+      });
+      after('hammer time!', function() {
+        t.stop();
+      });
+
+      it('setup: confirm the timer is running', function() {
+        assert(t.isActive());
+      });
+      it('emits a "start" event', function() {
+        t.set(800);
+        // this is set to 2 b/c of `#set` on previous line and `#start` in
+        // the `before` block
+        assert.equal(timesStarted, 2);
+      });
+      it('emits a "stop" event', function() {
+        assert.equal(timesStopped, 1);
+      });
+    });
   });
 
 });
