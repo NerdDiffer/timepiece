fa4b976228eddfba2e7c3b88c17909140d5dc377
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-09-27 12:16:18 -0700

    Stopwatch implementation using Date. Squashed commit of the following:
    commit fc1ad3bb150e5a4fac2695d57b01b90dad5f9bea
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Sun Sep 27 12:00:40 2015 -0700
    
        Add several updates Stopwatch class
    
        Control precision with a class-level constant.
    
        Use a private method & class-level constant to calculate differences in
        elapsed time.
    
        Add a 'split' feature, for lap times. This will store times inside an
        instance while not stopping the clock. The '#split' method will only
        emit a split event. The subsequent behavior is separated by calling two
        different methods: '#updateTimestamp' and '#addSplit'. Finer control.
    
        On creating a timestamp, check that it doesn't already have timestamp.
    
        On update a timestamp, do not delete the existing timestamp. Instead,
        move the timestamp deletion to a separate public method, '#reset'. This
        will allow you to stop & resume the watch.
    
    commit cd5f87cdb4abb47fda5e37e60603c6743d05dc6e
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Sun Sep 27 11:59:01 2015 -0700
    
        Perform a strict equal check on start/stop spec
    
        Got to double check the types because the number is being converted to
        a string and back to a number, behind the scenes.
    
    commit fe27ab9d7c99054e3e165102e6f05e9f9a333efb
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Sun Sep 27 11:57:46 2015 -0700
    
        Add spec for split/lap feature
    
        Follows the same pattern as the spec for starting & stopping, while
        checking the elapsed property.
    
    commit c0ea3f103055ea9655bd8e9dba295d045b430e3d
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Sun Sep 27 01:35:23 2015 -0700
    
        Add a behavioral spec from a cherry-picked commit
    
        The commit is 'e308dc8d2d'. But before committing, I updated the
        property names on the spec.
    
    commit 507d33bff588df13c678683b0981cd612c55772b
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Sun Sep 27 01:30:49 2015 -0700
    
        Experiment with Stopwatch, using Date class
    
        Rather than using the 'tick' event, the timestamp system uses the Date
        class, provided by javascript. Then just do some simple subtraction to
        figure out elapsed time...
    
        Renamed the 'time' property to 'elapsed'.

--- spec/Stopwatch_spec.js
+++ spec/Stopwatch_spec.js
@@ -6,14 +6,86 @@ describe('Stopwatch', function() {
   describe('initial settings', function() {
     var s = new Stopwatch();
     it('starts at 0', function() {
-      assert.equal(s.time, 0);
+      assert.equal(s.elapsed, 0);
     });
     it('is not running', function() {
       assert(!s.isActive());
     });
-    it('actually has its "ms" property set to 10', function() {
-      // this means it will emit 100 'tick' events per second
-      assert.equal(s.ms, 10);
+  });
+
+  describe('scenario: splits and laps', function() {
+    var s = new Stopwatch();
+    var testInterval = 1000;
+    context('splitting 1 second after starting', function() {
+      before('start timer', function() {
+        s.start();
+      });
+      it('splits after 1 second', function(done) {
+        setTimeout(function stopAtInterval() {
+          s.split();
+          assert(s.isActive());
+          done();
+        }, testInterval);
+      });
+      it('has 1.00 seconds as elapsed time', function() {
+        assert.strictEqual(s.elapsed, 1.00);
+      });
+      it('has this split time', function() {
+        assert.deepEqual(s.laps, [1.00]);
+      });
+    });
+
+    context('run for 1.00 second and split again', function() {
+      it('splits after 1.0 second', function(done) {
+        setTimeout(function stopAtInterval() {
+          s.split();
+          assert(s.isActive());
+          done();
+        }, testInterval);
+      });
+      it('now has 2.00 seconds as elapsed time', function() {
+        assert.strictEqual(s.elapsed, 2.00);
+      });
+      it('has these split times', function() {
+        assert.deepEqual(s.laps, [1.00, 2.00]);
+      });
+    });
+  });
+
+  describe('scenario: starting & stopping', function() {
+    var s = new Stopwatch();
+    var testInterval = 1500;
+
+    context('stopping 1.5 seconds after starting', function() {
+      before('start timer', function() {
+        s.start();
+      });
+      it('stops after 1.5 seconds', function(done) {
+        setTimeout(function stopAtInterval() {
+          s.stop();
+          assert(!s.isActive());
+          done();
+        }, testInterval);
+      });
+      it('has 1.50 seconds as elapsed time', function() {
+        assert.strictEqual(s.elapsed, 1.50);
+      });
+    });
+
+    context('starting again: run for 1.50 seconds and stop', function() {
+      before('start timer', function() {
+        s.start();
+      });
+      it('stops after 1.5 seconds', function(done) {
+        setTimeout(function stopAtInterval() {
+          s.stop();
+          assert(!s.isActive());
+          done();
+        }, testInterval);
+      });
+      it('now has 3.00 seconds as elapsed time', function() {
+        assert.strictEqual(s.elapsed, 3.00);
+      });
     });
   });
 });
