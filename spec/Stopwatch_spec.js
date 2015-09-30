466070c050e22dd5d14edc8b59c31b54bc916fec
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-09-29 22:41:10 -0700

    Simplify Stopwatch#stop feature. Refactor Stopwatch. Squashed commit of the following:
    commit c78c5633e170910eea6c803f57c2c6f7543cbdad
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Tue Sep 29 22:38:04 2015 -0700
    
        DRY up two instance methods for Stopwatch
    
        Simply call a new private method instead of writing up same line of
        code twice.
    
    commit 965a6c8b8e86416890398ab01d099c14e99e28fa
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Tue Sep 29 22:34:19 2015 -0700
    
        Update Stopwatch#stop to pass new specs
    
        Proceed whether or not it has a timestamp.
        Inside the 'update' event listener, call '#reset', instead of trying to
        overwrite the timestamp.
    
    commit 42a732fdc6bac6d6a42367d4257353bc1fd05ba8
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Tue Sep 29 22:28:06 2015 -0700
    
        Add a spec for Stopwatch#stop
    
        Check all the things.
    
    commit 76cdcc5c0ae95e450a7d1cd303fe75ba31a39f8c
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Tue Sep 29 22:23:47 2015 -0700
    
        Add spec to reset a stopwatch on call to '#stop'
    
        This feature is being simplified for now. The specification was changed
        to reset the 'elapsed' time when you call '#stop', then '#start'.
    
        Clarify some spec wording and remove extraneous console.log. Some
        commands had to be wrapped up in an 'it' block assertion, in order to
        ensure that the code is run.
    
    commit c5cac4749dd2e67669d706df804fbb401711b15f
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Tue Sep 29 16:28:57 2015 -0700
    
        Add a monolothic mess of changes
    
        Need to fix the elapsed time, tests all pass except for last one...
    
    commit 5037f235f57803ddc5c33952f37c53162b54436f
    Author: Rafael Espinoza <rafael@rafaelespinoza.com>
    Date:   Tue Sep 29 14:39:51 2015 -0700
    
        Name event listener functions for Stopwatch
    
        This change will make debugging and stack traces a little easier
        to follow.

--- spec/Stopwatch_spec.js
+++ spec/Stopwatch_spec.js
@@ -11,6 +11,125 @@ describe('Stopwatch', function() {
     it('is not running', function() {
       assert(!s.isActive());
     });
+    it('emits a tick event every 1 second', function() {
+      assert.equal(s.ms, 1000);
+    });
+  });
+
+  describe('#start', function() {
+    var s = new Stopwatch();
+    context('checking conditions before calling "#start"', function() {
+      it('is not active', function() {
+        assert(!s.isActive());
+      });
+      it('does not have a timestamp', function() {
+        assert(!s.hasOwnProperty('timestamp'));
+      });
+      it('has 0 elapsed time', function() {
+        assert.equal(s.elapsed, 0);
+      });
+    });
+    context('after calling "#start"', function() {
+      var started = false;
+      s.on('start', function hasStarted() {
+        started = true;
+      });
+      before('start timer', function() {
+        s.start();
+      });
+      it('emits the start event', function() {
+        assert(started);
+      });
+      it('is active', function() {
+        assert(s.isActive());
+      });
+      it('does have a timestamp', function() {
+        assert(s.hasOwnProperty('timestamp'));
+      });
+      it('has some elapsed time', function() {
+        s.updateElapsed();
+        assert(s.elapsed > 0);
+      });
+    });
+  });
+
+  describe('#stop', function() {
+    var s = new Stopwatch();
+    before('start timer', function() {
+      s.start();
+    });
+    context('checking conditions before calling "#stop"', function() {
+      it('is active', function() {
+        assert(s.isActive());
+      });
+      it('does have a timestamp', function() {
+        assert(s.hasOwnProperty('timestamp'));
+      });
+      it('has some elapsed time', function(done) {
+        setTimeout(function waitMoment() {
+          s.updateElapsed();
+          assert(s.elapsed > 0);
+          done();
+        }, 500);
+      });
+    });
+    context('after calling "#stop"', function() {
+      var stopped = false;
+      s.on('stop', function hasStopped() {
+        stopped = true;
+      });
+      before('stop the timer', function() {
+        s.stop();
+      });
+      it('emits the stop event', function() {
+        assert(stopped);
+      });
+      it('is not active', function() {
+        assert(!s.isActive());
+      });
+      it('does not have a timestamp', function() {
+        assert(!s.hasOwnProperty('timestamp'));
+      });
+      it('has 0.5 seconds elapsed time', function() {
+        assert.equal(s.elapsed, 0.5);
+      });
+    });
+  });
+
+  describe('scenario: simply start & check elapsed', function() {
+    var s = new Stopwatch();
+    var testInterval = 1800;
+    before('start timer', function() {
+      s.start();
+    });
+    it('has started', function() {
+      assert(s.isActive());
+    });
+    it('has 1.8 seconds', function(done) {
+      setTimeout(function checkAfterInterval() {
+        s.updateElapsed();
+        assert.equal(s.elapsed, 1.8);
+        done();
+      }, testInterval);
+    });
+    it('has 3.6 seconds', function(done) {
+      setTimeout(function checkAfterInterval() {
+        s.updateElapsed();
+        assert.equal(s.elapsed, 3.6);
+        done();
+      }, testInterval);
+    });
+    it('has 5.4 seconds', function(done) {
+      setTimeout(function checkAfterInterval() {
+        s.updateElapsed();
+        assert.equal(s.elapsed, 5.4);
+        done();
+      }, testInterval);
+    });
+    it('stops', function() {
+      s.stop();
+      assert(!s.isActive());
+    });
   });
 
   describe('scenario: splits and laps', function() {
@@ -21,7 +140,7 @@ describe('Stopwatch', function() {
         s.start();
       });
       it('splits after 1 second', function(done) {
-        setTimeout(function stopAtInterval() {
+        setTimeout(function splitAtInterval() {
           s.split();
           assert(s.isActive());
           done();
@@ -37,7 +156,7 @@ describe('Stopwatch', function() {
 
     context('run for 1.00 second and split again', function() {
       it('splits after 1.0 second', function(done) {
-        setTimeout(function stopAtInterval() {
+        setTimeout(function splitAtInterval() {
           s.split();
           assert(s.isActive());
           done();
@@ -52,7 +171,7 @@ describe('Stopwatch', function() {
     });
   });
 
-  describe('scenario: starting & stopping', function() {
+  describe('scenario: starting, stopping & resetting', function() {
     var s = new Stopwatch();
     var testInterval = 1500;
 
@@ -72,19 +191,29 @@ describe('Stopwatch', function() {
       });
     });
 
-    context('starting again: run for 1.50 seconds and stop', function() {
-      before('start timer', function() {
-        s.start();
+    context('reset: run for 1.50 seconds and stop', function() {
+      // wait before starting again
+      it('stops for 0.5 seconds', function(done) {
+        setTimeout(function wait() {
+          assert(!s.isActive());
+          done();
+        }, 500);
       });
-      it('stops after 1.5 seconds', function(done) {
+      it('starts afterwards', function() {
+        s.start();
+        assert(s.isActive());
+      });
+      it('stops after another 1.5 seconds', function(done) {
         setTimeout(function stopAtInterval() {
           s.stop();
           assert(!s.isActive());
           done();
         }, testInterval);
       });
-      it('now has 3.00 seconds as elapsed time', function() {
-        assert.strictEqual(s.elapsed, 3.00);
+      it('has 1.50 seconds as elapsed time', function() {
+        // evidence the stopwatch resets its 'elapsed' time property
+        // on call to '#stop'
+        assert.strictEqual(s.elapsed, 1.50);
       });
     });
   });
